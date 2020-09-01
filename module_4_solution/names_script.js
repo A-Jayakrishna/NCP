0bc73d77f6fb7be2b3800a88ce0c9c10dd6c329b
A-Jayakrishna <53211832+A-Jayakrishna@users.noreply.github.com>
2020-09-01 16:51:43 +0530

    Update names_script.js

--- module_4_solution/names_script.js
+++ module_4_solution/names_script.js
@@ -3,9 +3,9 @@
     for (var i = 0; i < names.length; i++) {
         var start_letter = names[i].charAt(0).toLowerCase();
         if (start_letter === 'j'||start_letter === 'J') {
-            byefunc.speak(names[i]);
+            byeSpeaker.speak(names[i]);
         } else {
-            hellofunc.speak(names[i]);
+            helloSpeaker.speak(names[i]);
         }
     }
 })();
