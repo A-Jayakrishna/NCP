e600b0c01b1acd9e4c053eaf61c883b4d8861713
A-Jayakrishna <53211832+A-Jayakrishna@users.noreply.github.com>
2020-09-01 16:43:58 +0530

    Update names_script.js

--- module_4_solution/names_script.js
+++ module_4_solution/names_script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Reviewer", "jayakrishna","Jasvith", "jordan", "michel", "Jackson", "spiderman", "nolan", "stark", "James bond", "jack","ajkc","bloodhund","caustic","octane"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var start_letter = names[i].charAt(0).toLowerCase();
         if (start_letter === 'j'||start_letter === 'J') {
